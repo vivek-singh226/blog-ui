351990abadaa6feed9fb1389b672d5f98aa5bd98
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-09-25 09:17:03 +0530

    Adding the support of fonts [Inter]

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import theme from './theme';
-import App from './App';
+import { CssBaseline } from "@mui/material"
+import { ThemeProvider } from "@mui/material/styles"
+import * as React from "react"
+import * as ReactDOM from "react-dom/client"
+import App from "./App"
+import theme from "./theme"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+   <React.StrictMode>
+      <ThemeProvider theme={theme}>
+         <CssBaseline />
+         <App />
+      </ThemeProvider>
+   </React.StrictMode>
+)
